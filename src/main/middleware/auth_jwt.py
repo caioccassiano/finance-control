7df5ccc570aec274e86bba9a7022a41a0e2c2e7a
caioccassiano <caioccassiano@icloud.com>
2025-06-27 09:17:04 +0200

    feat: creating auth decorator

--- src/main/middleware/auth_jwt.py
+++ src/main/middleware/auth_jwt.py
@@ -1,19 +1,27 @@
-from flask import request
+from flask import request, g
+from functools import wraps
 from src.drivers.jwt_handler import JwtHandler
 
-def auth_verify_jwt():
-  jwt_handler = JwtHandler()
-  raw_token = request.headers.get("Authorization")
-  user_id = request.headers.get("uid")
 
-  if not raw_token or not user_id:
-    raise Exception("Invalid inputs")
+def auth_verify_jwt(f):
+
+  @wraps(f)
+  def wrapper(*args, **kwargs):
+    jwt_handler = JwtHandler()
+    raw_token = request.headers.get("Authorization")
+    user_id = request.headers.get("uid")
+
+    if not raw_token or not user_id:
+      raise Exception("Invalid inputs")
+    
+    token = raw_token.split()[1]
+    token_informations = jwt_handler.decode_jwt_token(token)
+    token_uid = token_informations["user_id"]
+
+    if user_id and token_uid and (int(token_uid) == int(user_id)):
+      g.token_informations = token_informations
+      return f(*args, **kwargs)
+    raise Exception("User Unauthorized!")
+  return wrapper
   
-  token = raw_token.split()[1]
-  token_informations = jwt_handler.decode_jwt_token(token)
-  token_uid = token_informations["user_id"]
-
-  if user_id and token_uid and (int(token_uid) == int(user_id)):
-    return token_informations
-  raise Exception("User Unauthorized!")
 
