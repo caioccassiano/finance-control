90c7b1a20b64ed39f76a26319ca196d24f118d51
caioccassiano <caioccassiano@icloud.com>
2025-06-27 16:23:16 +0200

    feat: setting issues on password_handler (enconding hashed_password to check on the login)

--- src/drivers/password_handler.py
+++ src/drivers/password_handler.py
@@ -3,11 +3,12 @@ import bcrypt
 class PasswordHandler:
   def encrypt_password(self, password:str)->str:
     salt = bcrypt.gensalt()
-    hashed_password = bcrypt.hashpw(password.encode("utf-8"), salt=salt)
+    hashed = bcrypt.hashpw(password.encode("utf-8"), salt=salt)
+    hashed_password = hashed.decode("utf-8")
     return hashed_password
   
-  def check_password(self, password:str, hashed_password:str):
-    return bcrypt.checkpw(password.encode("utf-8"), hashed_password)
+  def check_password(self, password:str, hashed_password:str)->bool:
+    return bcrypt.checkpw(password.encode("utf-8"), hashed_password.encode("utf-8"))
   
 
 
